30fc50d0a65549bb6a7bf23e8faf027077ba4567
Koen Hufkens <koen.hufkens@gmail.com>
2024-03-04 19:06:56 +0100

    Update download_cmip6.sh

--- download_cmip6.sh
+++ download_cmip6.sh
@@ -4,7 +4,9 @@
 RSYNC_PASSWORD=useatyourownrisk rsync -vrlpt cmip6ng@atmos.ethz.ch::cmip6-ng > /tmp/cmip_file_list.txt
 
 # select the required files (append to file)
+# stack grep commands to filter to the desired level
 cat cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "pr_day" > download_list.txt
 cat cmip_file_list.txt | grep "r1i1p1f1_native" | grep "CESM2_historical" | grep "tas_day" >> download_list.txt
 
+# download using the temporary list, into the current directory
 SYNC_PASSWORD=useatyourownrisk rsync -vrlpt --files-from=download_list.txt cmip6ng@atmos.ethz.ch::cmip6-ng cmip6-ng
